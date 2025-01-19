88b87dcce66e6548a4b683201569c730bb1aede8
ElCresp0 <jacky8203@protonmail.com>
2025-01-19 15:15:06 +0100

    update scene to use CollsionDetector

--- Assets/Scripts/Shrimp Scripts/CollsionDetector.cs
+++ Assets/Scripts/Shrimp Scripts/CollsionDetector.cs
@@ -7,23 +7,23 @@ public class CollsionDetector : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    // Funkcja wywo³ywana przy kolizji
+    // Funkcja wywoï¿½ywana przy kolizji
     private void OnCollisionEnter(Collision collision)
     {
-        // SprawdŸ, czy nazwa obiektu to "Potion_Red"
+        // Sprawdï¿½, czy nazwa obiektu to "Potion_Red"
         if (collision.gameObject.name == "Potion_Red")
         {
-            Debug.Log("Potion_Red zosta³ dodany do kocio³ka!");
-            // Zniszcz obiekt, z którym nast¹pi³a kolizja
+            Debug.Log("Potion_Red zostaï¿½ dodany do kocioï¿½ka!");
+            // Zniszcz obiekt, z ktï¿½rym nastï¿½piï¿½a kolizja
             Destroy(collision.gameObject);
         }
     }
